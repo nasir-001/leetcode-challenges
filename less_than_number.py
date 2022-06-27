c1992e1f145d90de33be9f3c89721a47dc1777b1
Nasir Lawal <nasirlawal001@gmail.com>
2022-06-27 21:31:06 +0100

    solve less than current number

--- less_than_number.py
+++ less_than_number.py
@@ -1,11 +1,30 @@
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+#         output = []
+#         for i in range(len(nums)):
+#             count = 0
+#             for j in range(len(nums)):
+#                 if j != i and nums[j] < nums[i]:
+#                     count += 1
+#             output.append(count)
+
+#         return output
+
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        smaller_dict = {}
+        sorted_nums = sorted(nums, reverse=True)
+        for i in range(len(sorted_nums) - 1):
+            curr_num = sorted_nums[i]
+            next_num = sorted_nums[i+1]
+            if next_num < curr_num:
+                remaining_values = len(sorted_nums) - (i + 1)
+                smaller_dict[curr_num] = remaining_values
+
+        smaller_dict[sorted_nums[-1]] = 0
+
         output = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if j != i and nums[j] < nums[i]:
-                    count += 1
-            output.append(count)
+        for num in nums:
+            output.append(smaller_dict[num])
 
         return output
