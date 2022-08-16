f3523248d416952b517f5128c2fb07bc99dfb586
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-16 14:01:11 +0100

    solved is same tree

--- rough.py
+++ rough.py
@@ -1,6 +1,22 @@
-def main():
-    list1 = [1,2,4]
-    list2 = [1,3,4]
-    
-if __name__ == '__main__':
-    main()
+import math
+
+def time(n):
+    """ Return the number of steps 
+    necessary to calculate
+    `print countdown(n)`"""
+    steps = 0
+    for i in range(n):
+        steps = steps * math.ceil(n/5.0)
+        if steps == 0:
+            steps += 1
+    print(steps)
+print(time(6))
+
+def countdown(x):
+    y = 0
+    while x > 0:
+        x = x - 5
+        y = y + 1
+    print(y)
+
+print(countdown(50))
