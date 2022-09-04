bf22ab100f1b3936b8c8a8d03e3bc8319052af66
Nasir Lawal <nasirlawal001@gmail.com>
2022-09-04 20:57:37 +0100

    fixed parse cvg javascript-version

--- coding challenge/parseCsv.js
+++ coding challenge/parseCsv.js
@@ -1,48 +1,48 @@
 function parseCSV(input, separator, quote) {
-    separator = separator || ',';
-    quote = quote || '"';
-  
-    var data = [];
-    var row = [];
-    var value = [];
-    var inString = false;
-  
-    function pushValue() {
-      row.push(value.join(""));
-      value = [];
-    }
-  
-    function pushRow() {
-      pushValue();
-      data.push(row);
-      row = [];
-    }
-  
-    for(var i=0; i<input.length; i++) {
-      var ch = input[i];
-      if(inString) {
-        if(ch === quote) {
-          if(input[i+1] === quote) {
-            value.push(quote);
-            i++;
-          } else {
-            inString = false;
-          }
+  separator = separator || ',';
+  quote = quote || '"';
+
+  var data = [];
+  var row = [];
+  var value = [];
+  var inString = false;
+
+  function pushValue() {
+    row.push(value.join(""));
+    value = [];
+  }
+
+  function pushRow() {
+    pushValue();
+    data.push(row);
+    row = [];
+  }
+
+  for(var i=0; i<input.length; i++) {
+    var ch = input[i];
+    if(inString) {
+      if(ch === quote) {
+        if(input[i+1] === quote) {
+          value.push(quote);
+          i++;
         } else {
-          value.push(ch);
+          inString = false;
         }
       } else {
-        if(ch === '\n') {
-          pushRow();
-        } else if(ch === separator) {
-          pushValue();
-        } else if(ch === quote) {
-          inString = true;
-        } else {
-          value.push(ch);
-        }
+        value.push(ch);
+      }
+    } else {
+      if(ch === '\n') {
+        pushRow();
+      } else if(ch === separator) {
+        pushValue();
+      } else if(ch === quote) {
+        inString = true;
+      } else {
+        value.push(ch);
       }
     }
-    pushRow()
-    return data
+  }
+  pushRow()
+  return data
 }
